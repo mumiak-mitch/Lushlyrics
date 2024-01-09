f145236a0545b7edd2d6efda9f66f4d9ec48b524
mumiak-mitch <mumiaktech@gmail.com>
2024-01-10 00:27:22 +0300

    Login and Forgot password logics

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ To get started, follow these steps:
 
 ```sh
 git clone https://github.com/mumiak-mitch/Lushlyrics.git
-cd lushlyrics-webapp-django
 ```
 
 ### 2. Create and activate a virtual environment
@@ -35,7 +34,7 @@ Navigate to [http://127.0.0.1:8000/](http://127.0.0.1:8000/) in your web browser
 
 Restrict anonymous access to the Lushlyrics website.
 
-Secure access to the site with authentication and authorization  
+Secure access to the site with authentication and authorization
 
 Design user-friendly password recovery.
 
@@ -46,3 +45,7 @@ I forked the Lushlyrics-insecure repository so that I can contribute to it.
 My contributions are as shown above.
 
 A user can access the site and play music even while not logged in but they can't access some features like liked songs unless they are logged in.
+
+### Users
+
+Admin, Youttify1234
---
+++ main/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path("", views.default, name='default'),
+    path("playlist/", views.playlist, name='your_playlists'),
+    path("search/", views.search, name='search_page'),
+
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout-page/', views.logoutPage, name="logout-page"),
+
+    path('login/', views.login_view, name='login'),
+ 
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', views.password_reset_confirm_view, name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+]
